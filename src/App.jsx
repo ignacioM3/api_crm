570620df39ba8d1dde58dbf8263503e1ecfd2fb4
ignacioM3 <ignacio.marquez159@gmail.com>
2022-06-06 15:19:02 -0300

    agrego inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from "./layout/Layout"
 import EditarClientes from "./pages/EditarClientes"
+import Home from "./pages/Home"
 import Inicio from "./pages/Inicio"
 import NuevoClientes from "./pages/NuevoClientes"
 import VerCliente from './pages/VerCliente'
 
 function App() {
-
+  
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />}/>
+          
         <Route path="/clientes" element={<Layout/>}>
             <Route index element={<Inicio />} />
             <Route path="nuevo" element={<NuevoClientes/>} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-blue-700 min-h-screen pt-20 '>
+
+        <div className='bg-white p-5 text-center w-2/4 mx-auto rounded-lg'>
+            <h1 className='text-3xl font-bold my-5'>Bienvenido! CRM - REACT</h1>
+            <a href="https://github.com/ignacioM3" className='block text-xl font-bold text-indigo-500 hover:text-indigo-700' target="__blank">IgnacioM3 <i class="fa-brands fa-github"></i></a>
+            <button 
+            type='button' 
+            className='py-3 px-8 sm:px-10 my-8 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg'
+            >
+                Inicio
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
